34a0e46e415327c7dbc41fc25682325793c18337
naveen7CR <naveenshubham620@gmail.com>
2026-02-22 20:00:51 +0530

    data structure py

---
+++ data structure.py
@@ -0,0 +1,61 @@
+# import array as ar
+
+# arrayName = ar.array('i',[12,23,34,45])
+# print(arrayName)
+
+
+# #append(value)
+# arrayName.append(24)
+# print(arrayName)
+
+# #insert(index,value)
+# arrayName.insert(0,10)
+# print(arrayName)
+
+# #Travesal - Access all the values
+# for val in arrayName:
+#     print(val)
+
+
+# for i in range(0, len(arrayName)):
+#     print(arrayName[i], end=" ")
+
+# #delete
+# #1. remove(value)
+# arrayName.remove(12)
+# print(arrayName)
+
+# #2.pop() or pop(index)
+# print(arrayName.pop())
+# print(arrayName.pop(0))
+# print(arrayName)
+
+# n=int(input('Enter number'))
+# count=0
+# for i in arrayName:
+#     if i==n:
+#         count+=1
+# print(f"occurence={count}")
+
+# matrix= [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9],
+# ]
+
+# rows= len(matrix)
+# col= len(matrix[0])
+
+# print("Column-wise traversal using nested loops:")
+# for j in range(cols):      
+#     for i in range(rows):  
+#         print(matrix[i][j], end=" ")
+#     print() 
+
+ 
+
+    
+    
+    
+
+
